3f6ecef9ff387ca7702573dbbb222c6919f8e8d4
ethanbg2 <ethanbg2@illinois.edu>
2023-07-29 17:04:34 -0500

    set up resume object store

--- src/store/app.js
+++ src/store/app.js
@@ -1,8 +1,9 @@
 // Utilities
 import { defineStore } from 'pinia'
+import resume from '@/assets/resume.json'
 
 export const useAppStore = defineStore('app', {
   state: () => ({
-    //
+    resume: resume
   }),
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,10 +4,21 @@
 
 <script>
   import HelloWorld from '@/components/HelloWorld.vue'
+  import {useAppStore} from '@/store/app'
+
   export default {
     name: "HomeView",
     components: {
       HelloWorld
+    },
+    setup() {
+      const store = useAppStore();
+      return {
+        store
+      }
+    },
+    created() {
+      console.log(this.store.resume.basics.name);
     }
   }
 </script>
