4eda6e29c1ebf832c8b2d0ec1400c604319be784
ethanbg2 <ethanbg2@illinois.edu>
2023-07-29 23:33:33 -0500

    add lottie file

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,19 @@
   <!-- <HelloWorld /> -->
   <v-container class="fill-height">
     <v-responsive class="align-center text-left fill-height">
-      <v-col cols="auto">
+      <v-row>
+        <v-col cols="auto">
         <div class="text-h2 font-weight-bold">
           Hi, I'm Ethan.
         </div>
-        <div class="text-h5 font-weight-thin">
+        <div class="text-h5 font-weight-thin mt-4">
           {{ store.resume.basics.summary }}
         </div>
         <v-row class="mt-2">
           <v-btn 
             width="175px" 
             height="50px" 
-            class="ml-2 my-4 rounded-lg" 
+            class="ml-3 my-4 rounded-lg" 
             color="primary"
             to="/work">
             Check Out My Work
@@ -21,13 +22,15 @@
           <v-btn 
             width='175px' 
             height="50px" 
-            class="ml-2 my-4 rounded-lg button-hover" 
+            class="ml-2 my-4 rounded-lg" 
             color="primary"
             :href="store.resume.basics.resume_url">
             Download Resume
           </v-btn>
-        </v-row>
-      </v-col>
+          </v-row>
+        </v-col>
+        <Vue3Lottie :animationData="monkeyLottie" height="350px" width="350px" />
+      </v-row>
     </v-responsive>
   </v-container>
 </template>
@@ -35,11 +38,20 @@
 <script>
   import HelloWorld from '@/components/HelloWorld.vue'
   import {useAppStore} from '@/store/app'
+  import { Vue3Lottie } from 'vue3-lottie'
+  import meditatingMonkey from '@/assets/meditating-monkey.json';
 
   export default {
     name: "HomeView",
     components: {
-      HelloWorld
+      HelloWorld,
+      Vue3Lottie
+    },
+    data() {
+      return {
+        monkeyLottie: meditatingMonkey
+      }
+
     },
     setup() {
       const store = useAppStore();
