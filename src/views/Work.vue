2069336316dc7934dcdec3b17a0d35a726e92521
ethanbg2 <ethanbg2@illinois.edu>
2023-07-30 13:46:32 -0500

    started work page

--- src/views/Work.vue
+++ src/views/Work.vue
@@ -1,7 +1,34 @@
 <template>
-    <h1>
-        Work
-    </h1>
+    <v-container>
+        <v-responsive>
+                <v-col cols="auto">
+                    <div class="text-h2 text-center">
+                        My Work
+                    </div>
+                    <v-select class="my-6"
+                    v-model="selectedSkills"
+                    :items="skills"
+                    chips
+                    label="Skills"
+                    multiple
+                    clearable
+                    hint="Filter My Work by Skills"
+                    persistent-hint
+                    ></v-select>
+                    <div class="text-h4 text-center my-5">
+                        Projects
+                    </div>
+                    <div class="text-h4 text-center my-5">
+                        Work Experience
+                    </div>
+                    <div class="text-h4 text-center my-5">
+                        Courses
+                    </div>
+                </v-col>
+
+        </v-responsive>
+    </v-container>
+
 </template>
   
 <script>
@@ -11,6 +38,14 @@ export default {
     name: "WorkView",
     components: {
     
+    },
+
+    data() {
+        return {
+            skills: ['test', 'test2', 'test3'],
+            selectedSkills: []
+        }
+
     },
     setup() {
         const store = useAppStore();
