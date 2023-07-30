e064ae9529ba6dfbb2c507a57a41570f13bfb1b5
ethanbg2 <ethanbg2@illinois.edu>
2023-07-29 20:33:43 -0500

    add profile links

--- src/layouts/default/Navigation.vue
+++ src/layouts/default/Navigation.vue
@@ -7,17 +7,73 @@
         <v-list>
         <v-list-item
             prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
-            title="Ethan Grinberg"
-            subtitle="Software Developer"
+            :title="store.resume.basics.name"
+            :subtitle="store.resume.basics.label"
             to="/"
+            :active="false"
         ></v-list-item>
         </v-list>
 
         <v-divider></v-divider>
 
-        <v-list density="compact" nav>
-        <v-list-item prepend-icon="mdi-information-outline" title="About Me" value="about" to="/about"></v-list-item>
-        <v-list-item prepend-icon="mdi-code-tags" title="My Work" value="work" to="/work"></v-list-item>
+        <v-list nav>
+            <v-list-item 
+                :active="false" 
+                prepend-icon="mdi-information-outline" 
+                title="About Me" 
+                value="about" 
+                to="/about"></v-list-item>
+            <v-list-item 
+                :active="false" 
+                prepend-icon="mdi-code-tags" 
+                title="My Work" 
+                value="work" 
+                to="/work"></v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+        <v-list>
+            <v-list-item
+                prepend-icon="mdi-github" 
+                title="Github" 
+                value="github" 
+                :active="false"
+                :href="github"
+            ></v-list-item>
+            <v-list-item
+                prepend-icon="mdi-linkedin" 
+                title="Linkedin" 
+                value="linkedin" 
+                :active="false"
+                :href="linkedin"
+            ></v-list-item>
+            <v-list-item
+                prepend-icon="mdi-mail" 
+                title="Email" 
+                value="email" 
+                :active="false"
+                :href="`mailto:${store.resume.basics.email}`"
+            ></v-list-item>
         </v-list>
     </v-navigation-drawer>
 </template>
+
+<script>
+import {useAppStore} from '@/store/app'
+
+export default {
+    name: "Navigation",
+    setup() {
+      const store = useAppStore();
+      const profiles = store.resume.basics.profiles;
+      const github =  profiles.filter(item => item.network === 'GitHub')[0].url
+      const linkedin = profiles.filter(item => item.network ==='LinkedIn')[0].url
+      return {
+        store,
+        github,
+        linkedin
+      }
+    }
+
+}
+</script>
