b502e88815467c50648659fb5cdb5052b62240b6
ethanbg2 <ethanbg2@illinois.edu>
2023-08-12 19:13:34 -0500

    add list of contributions

--- src/views/ProjectPage.vue
+++ src/views/ProjectPage.vue
@@ -1,9 +1,68 @@
 <template>
   <v-container>
     <v-responsive>
-      <h2>
-        Links
-      </h2>
+      <div
+        class="text-start text-h4 mt-5"
+      >
+        {{ is_work ? data.position : data.name }}
+      </div>
+      <div class="d-flex justify-start align-center mt-5">
+        <div class="d-flex align-center">
+          <v-icon
+            icon="mdi-clock"
+            class="mr-2"
+          />
+          <div class="text-h6">
+            {{ data.date }}
+          </div>
+        </div>
+        <div class="d-flex align-center ml-5">
+          <v-icon
+            icon="mdi-pound"
+            class="mr-2"
+          />
+          <div class="text-h6">
+            {{ is_work ? data.company : data.type }}
+          </div>
+        </div>
+      </div>
+      <v-chip-group column>
+        <v-chip
+          v-for="skill in data.skills"
+          :key="skill"
+        >
+          {{ skill }}
+        </v-chip>
+      </v-chip-group>
+      <v-divider class="mt-5" />
+      <v-container v-if="!is_work">
+        <v-row>
+          <v-col
+            v-for="image in data.images"
+            :key="image"
+            cols="auto"
+          >
+            <img
+              :src="getImg(image)"
+              style="width: 300px; height: 300px; object-fit: cover; border-radius: 8px;"
+            >
+          </v-col>
+        </v-row>
+      </v-container>
+      <div class="text-h4">
+        Contributions
+      </div>
+      <v-divider />
+      <ul>
+        <li
+          v-for="(item, index) in data.contributions"
+          :key="index"
+          class="mt-5 text-h6 font-weight-light ml-5"
+          style="max-width: 900px;"
+        >
+          {{ item }}
+        </li>
+      </ul>
     </v-responsive>
   </v-container>
 </template>
@@ -41,7 +100,17 @@
           this.data = projObj[0];
         }
         console.log(this.data);
+      },
+      methods: {
+        getImg(name) {
+            return new URL(`../assets/${name}`, import.meta.url).href;
+        }
       }
     }
   </script>
   
+<style scoped>
+.contributions-list {
+
+}
+</style>
